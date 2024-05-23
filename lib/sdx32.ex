64bb37f1b9eda760e0f90f804c8692f39051de76
Adrian Irving-Beer <wisq@wisq.net>
2024-05-23 14:19:34 -0400

    `mix new sdx32`

---
+++ lib/sdx32.ex
@@ -0,0 +1,18 @@
+defmodule Sdx32 do
+  @moduledoc """
+  Documentation for `Sdx32`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sdx32.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sdx32_test.exs
@@ -0,0 +1,8 @@
+defmodule Sdx32Test do
+  use ExUnit.Case
+  doctest Sdx32
+
+  test "greets the world" do
+    assert Sdx32.hello() == :world
+  end
+end
