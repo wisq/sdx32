0a448cdfb3dabbb8a73d40413d82736330cce7af
Adrian Irving-Beer <wisq@wisq.net>
2024-06-01 02:26:43 -0400

    Gracefully exit on websocket disconnect.

--- lib/sdx32/socket.ex
+++ lib/sdx32/socket.ex
@@ -1,8 +1,12 @@
 defmodule Sdx32.Socket do
   use WebSockex
+  require Logger
 
   alias Sdx32.Parameters
   alias Sdx32.Event
+
+  @prefix "[#{inspect(__MODULE__)}] "
+  @max_reconnects 3
 
   def start_link(opts) do
     {%Parameters{} = params, opts} = Keyword.pop!(opts, :params)
@@ -18,10 +22,31 @@ defmodule Sdx32.Socket do
   end
 
   @impl true
-  def handle_connect(_conn, %Parameters{} = params) do
-    IO.puts("Connected")
+  def handle_connect(conn, %Parameters{} = params) do
+    Logger.info(@prefix <> "Connected to #{log_peer(conn)}")
     send(self(), :register)
     {:ok, params}
+  end
+
+  @impl true
+  def handle_disconnect(%{reason: {:remote, 1000, _}}, params) do
+    Logger.info(@prefix <> "Stream Deck software is shutting down, exiting normally.")
+    System.stop(0)
+    {:ok, params}
+  end
+
+  @impl true
+  def handle_disconnect(%{conn: conn, reason: reason, attempt_number: attempt}, params) do
+    Logger.error(@prefix <> "Lost connection to #{log_peer(conn)}: #{inspect(reason)}")
+
+    if attempt > @max_reconnects do
+      Logger.error(@prefix <> "Failed to reconnect after #{attempt - 1} attempts, shutting down.")
+      System.stop(1)
+      {:ok, params}
+    else
+      Logger.info(@prefix <> "Attempting to reconnect (#{attempt}) ...")
+      {:reconnect, params}
+    end
   end
 
   @impl true
@@ -37,5 +62,9 @@ defmodule Sdx32.Socket do
   def handle_info(:register, params) do
     json = %{event: params.register_event, uuid: params.plugin_uuid} |> Jason.encode!()
     {:reply, {:text, json}, params}
+  end
+
+  defp log_peer(%WebSockex.Conn{host: host, port: port, path: path}) do
+    "ws://#{host}:#{port}#{path}"
   end
 end
